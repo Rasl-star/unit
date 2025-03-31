39a1628939b9a8a2c151b5c4623627f357338158
Rasl test <tlevaliev94@gmail.com>
2025-03-31 17:48:59 +0400

    change

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,35 @@
+package ru.netology;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldReturnRemainIfAmountIsNotMultipleOfBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturnBoundaryIfAmountIsMultipleOfBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturnCorrectRemainForAmountGreaterThanBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 2500;
+        int actual = service.remain(amount);
+        int expected = 500;
+        Assert.assertEquals(actual, expected);
+    }
+}
+
