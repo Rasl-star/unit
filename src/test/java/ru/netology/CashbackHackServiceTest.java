c20f3da7bffefb9ab20f658c3eb501b30abc5d92
Rasl test <tlevaliev94@gmail.com>
2025-04-01 12:59:52 +0400

    new model creation

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,35 +1,42 @@
 package ru.netology;
-
-import org.testng.annotations.Test;
-import org.testng.Assert;
+import org.junit.Test;
+import org.junit.Assert;
 
 public class CashbackHackServiceTest {
 
     @Test
-    public void shouldReturnRemainIfAmountIsNotMultipleOfBoundary() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 900;
-        int actual = service.remain(amount);
-        int expected = 100;
-        Assert.assertEquals(actual, expected);
-    }
-
-    @Test
-    public void shouldReturnBoundaryIfAmountIsMultipleOfBoundary() {
+    public void shouldReturnZeroIfAmountIsMultipleOfBoundary() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
-        int actual = service.remain(amount);
         int expected = 0;
-        Assert.assertEquals(actual, expected);
+        int actual = service.remain(amount);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
-    public void shouldReturnCorrectRemainForAmountGreaterThanBoundary() {
+    public void shouldReturnCorrectRemainForNonMultipleAmount() {
         CashbackHackService service = new CashbackHackService();
-        int amount = 2500;
+        int amount = 750;
+        int expected = 250;
         int actual = service.remain(amount);
-        int expected = 500;
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnBoundaryMinusAmountIfSmallValue() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 200;
+        int expected = 800;
+        int actual = service.remain(amount);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnCorrectRemainForLargeNonMultipleAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 2345;
+        int expected = 655;
+        int actual = service.remain(amount);
+        Assert.assertEquals(expected, actual);
     }
 }
-
